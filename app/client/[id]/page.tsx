3fd12f443cac0123d9c013aa2defeb6b2274abb0
FragazBR <fragazmusic@gmail.com>
2025-07-18 13:04:57 -0300

    feat: adiciona ErrorBoundary para debug visual e no console do navegador na página admin

--- app/client/[id]/page.tsx
+++ app/client/[id]/page.tsx
@@ -1,5 +1,8 @@
 import DashboardClient from "@/components/dashboardClient";
+import { type Metadata, type ResolvingMetadata } from "next";
 
-export default function ClientPage({ params }: { params: { id: string } }) {
+type PageProps = { params: { id: string } };
+
+export default function ClientPage({ params }: PageProps) {
   return <DashboardClient clientId={params.id} />;
 }
